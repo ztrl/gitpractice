4c4ae4aab3c9e8cdd1ffa055933dc8d0a8fd0d8d
ztrl <ztrl@naver.com>
2022-11-15 09:14:37 +0900

    실행1 추가

--- src/App.java
+++ src/App.java
@@ -4,8 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("commit Test");
 		System.out.println("push Test");
-		System.out.println("마스터2");
-		System.out.println("마스터3");
+		System.out.println("마스터1");
 	}
 
 }
